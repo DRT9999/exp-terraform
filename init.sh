f5aacbc674c8c7ad6fede82e3479d9661153fb99
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-28 12:40:34 +0530

    proAn

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 terraform init -backend-config=env-dev/state.tfvars
-terraform plan -var-file=env-dev/main.tfvars ssh_pwd=${ssh_pwd} #-var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
+terraform plan -var-file=env-dev/main.tfvars ssh_pwd="AES:dT32hCnvveJA2n0MFS/O9w==:yIrj6vXbBFzJHR7qOMNt/w==" #-var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
 terraform ${action} -auto-approve ssh_pwd=${ssh_pwd} #-var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
